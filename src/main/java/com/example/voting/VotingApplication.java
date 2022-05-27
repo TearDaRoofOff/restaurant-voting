aa0827670251fdf039e3bd24f5c32efda41c860a
Ruslan Kolesnikov <kolesnikov.ruslan@gmail.com>
2022-05-27 18:29:02 +0700

    init

---
+++ src/main/java/com/example/voting/VotingApplication.java
@@ -0,0 +1,14 @@
+package com.example.voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class VotingApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/voting/model/AbstractBaseEntity.java
@@ -0,0 +1,51 @@
+package com.example.voting.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+// http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
+@Access(AccessType.FIELD)
+//@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, isGetterVisibility = NONE, setterVisibility = NONE)
+public abstract class AbstractBaseEntity {
+    public static final int START_SEQ = 100;
+
+    @Id
+    @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
+    @Setter
+    @Getter
+    protected Integer id;
+
+    protected AbstractBaseEntity() {
+    }
+
+    protected AbstractBaseEntity(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ":" + id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !getClass().equals(Hibernate.getClass(o))) {
+            return false;
+        }
+        AbstractBaseEntity that = (AbstractBaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
+    }
+}

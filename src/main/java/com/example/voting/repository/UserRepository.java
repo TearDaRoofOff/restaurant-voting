9f678a22eb6a65d7ad276b6ae13cccc36045ba96
Ruslan Kolesnikov <kolesnikov.ruslan@gmail.com>
2022-05-29 21:43:05 +0700

    was added HAL Explorer

--- src/main/java/com/example/voting/repository/UserRepository.java
+++ src/main/java/com/example/voting/repository/UserRepository.java
@@ -2,7 +2,12 @@ package com.example.voting.repository;
 
 import com.example.voting.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "юзер", path = "chosen-one")
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByEmail(@Param("email") String email);
 
 }
